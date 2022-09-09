79800e2ea18d57db04e34d9d353528f5652aa2e5
Evans Ochieng <evansochieng121@gmail.com>
2022-09-09 18:05:39 +0300

    Write getter and setter methods

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+
+// Shape calculator 
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+        this.pi = Math.PI
+    }
+
+    // Getter methods
+    get diameter(){
+        return this.radius * 2;
+    }
+
+    get circumference(){
+        return this.pi * this.diameter;
+    }
+
+    get area(){
+        return this.pi * this.radius**2;
+    }
+
+    // Setter methods
+    // Don't forget about PEMDAS
+    set diameter(diameter){
+        return this.radius = diameter / 2;
+    }
+
+    set circumference(circumference){
+        return this.radius = (circumference / this.pi) / 2;
+    }
+
+    set area(area){
+        return this.radius = Math.sqrt((area / this.pi));
+    }
+}
